1d52c976271a8f5352cf8d561650de041950f562
dariomicelii <dariomiceli01@gmail.com>
2024-10-22 17:38:11 +0200

    js attuale

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const lampadinaEl = document.getElementById("lampadina");
+const lampadinaAccesaEl = document.getElementById("lampadinaAccesa");
 const interruttoreEl = document.getElementById("interruttore");
 // const lampadinaAccesaEl= document.createElement("img")
 // lampadinaAccesaEl.src = "./img/yellow_lamp.png"
 
 interruttoreEl.addEventListener('click', () => {
-    lampadinaEl.classList.toggle("d-none")
+    lampadinaAccesaEl.classList.toggle("d-none")
 }); 
